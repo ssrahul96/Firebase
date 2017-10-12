271aa3ce938055e651fc869cdc7f15572d0644d9
ssrahul96 <ssrahul96@gmail.com>
2017-10-12 22:17:39 +0530

    deployed to beta.stsimproveyourenglish.com

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,15 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello from Firebase!");
 });
+
+exports.addnumbers = functions.https.onRequest((request, response) => {
+    var name = request.body.first_name;
+
+    var lname = request.body.last_name;
+
+
+    var num1 = request.body.num1;
+    var num2 = request.body.num2;
+    response.send("welcome : " + name + "<br>Father name : " + lname + "<br>Sum = " + num1 + num2);
+
+});
