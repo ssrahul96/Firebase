f19963fe785d7c0d2cbce850e533431e5a881ed3
Rahul S S <rahul.ss@gofrugal.com>
2018-02-16 15:05:44 +0530

    implemented express

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,11 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const app = express();
 
-
-exports.addnumbers = functions.https.onRequest((request, response) => {
-    var name = request.body.first_name;
-
-    var lname = request.body.last_name;
-
-
-    var num1 = request.body.num1;
-    var num2 = request.body.num2;
-    response.send("welcome : " + name + "<br>Father name : " + lname + "<br>Sum = " + num1 + num2);
+app.get('/random', (req, res) => {
+    res.json({"status":"success","message":Math.floor((Math.random() * 10) + 1)});
 });
 
-exports.getRandom = functions.https.onRequest((request,response) =>{
-    response.json({"status":"success","message":Math.floor((Math.random() * 10) + 1)});
-});
+
+exports.api = functions.https.onRequest(app);
